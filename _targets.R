d023ff9d0c2182151efdedd1454cb5c5a1198b55
Em Maloney <emily.k.maloney@duke.edu>
2022-11-07 14:23:16 +0000

    adding comments to targets file

--- _targets.R
+++ _targets.R
@@ -17,14 +17,19 @@ tar_option_set(packages = c("gtrendsR", "readr", "dplyr", "tidyr",
 
 
 list(
+  #first, read in the data
   tar_target(joe_diss_occs, "data/joe_diss_essential_data.csv",format = "file"),
   tar_target(occs_df, read_csv(joe_diss_occs)),
+  
+  #next, make dfs of search terms
   tar_target(request_dfs, map(occs_df$search_term, create_request_df)),
+  
+  #third, send the requests through
   #splitting up the requests because of the rate limiting issue 
   tar_target(first_5, map(request_dfs[1:5], send_request)),
   tar_target(second_5, map(request_dfs[6:10], send_request)),
   tar_target(third_5, map(request_dfs[11:15], send_request)),
-  #tar_target(fourth_5, map(request_dfs[16:20], send_request)),
+  tar_target(fourth_5, map(request_dfs[16:20], send_request)),
   tar_target(fifth_5, map(request_dfs[21:25], send_request)),
   tar_target(sixth_5, map(request_dfs[26:30], send_request)),
   tar_target(last_7, map(request_dfs[31:37], send_request))
